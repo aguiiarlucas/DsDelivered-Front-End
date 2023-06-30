2aa4915fd0e79ee7f2b85a2564b98c1aa1ec3c26
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2023-06-29 22:22:25 -0300

    Update again

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,79 +1,74 @@
 import { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
 import { OrderLocationData } from './types';
- 
-
 
 const initialPosition = {
-    lat: -21.2600538,
-    lng: -48.322614
+    lat: -21.3874457,
+    lng: -51.5917552
 }
-type Place = {
+
+type Place={
     label?: string;
     value?: string;
-    position:{
-        lat: number;
-        lng: number;
+    position: {
+        lat:number;
+        lng:number;
     };
-} 
- type Props = {
-    onChangeLocation: (location : OrderLocationData) => void;
- }
-function OrderLocation({onChangeLocation}: Props) {
-    const[address,setAddress] = useState<Place>({
-     position: initialPosition  
+}
+
+type Props = {
+    onChangeLocation: (location: OrderLocationData) => void;
+}
+
+function OrderLocation({ onChangeLocation }: Props) {
+
+    const [address, setAddress] = useState<Place>({
+        position: initialPosition
     });
 
     const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
         const response = await fetchLocalMapBox(inputValue);
-
+      
         const places = response.data.features.map((item: any) => {
-            return ({
-                label: item.place_name,
-                value: item.place_name,
-                position: {
-                    lat: item.center[1],
-                    lng: item.center[0]
-                },
-                place: item.place_name,
-            });
+          return ({
+            label: item.place_name,
+            value: item.place_name,
+            position: {
+              lat: item.center[1],
+              lng: item.center[0]
+            }
+          });
         });
-
+      
         callback(places);
-    };
-
-    const handleChangeSelect = (place: Place) => {
+      };
+      
+      const handleChangeSelect = (place: Place) => {
         setAddress(place);
         onChangeLocation({
-            latitude: place.position.lat,
-            longitude: place.position.lng,
-            address: place.label!
+          latitude: place.position.lat,
+          longitude: place.position.lng,
+          address: place.label!
         });
-    };
-
+      };
 
     return (
         <div className="order-location-container">
             <div className="order-location-content">
                 <h3 className="order-location-title">
                     Selecione onde o pedido deve ser entregue:
-            </h3>
+                </h3>
                 <div className="filter-container">
                     <AsyncSelect
                         placeholder="Digite um endereço para entregar o pedido"
-                        className=" filter"
+                        className="filter"
                         loadOptions={loadOptions}
                         onChange={value => handleChangeSelect(value as Place)}
-                />
+                    />
                 </div>
-
-                <MapContainer 
-                center={address.position}
-                 zoom={15} 
-                 key = {address.position.lat}
-                 scrollWheelZoom={false}>
+                <MapContainer center={address.position} zoom={13} key={address.position.lat} scrollWheelZoom>
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -88,9 +83,6 @@ function OrderLocation({onChangeLocation}: Props) {
             </div>
         </div>
     )
-
-
-
 }
 
 export default OrderLocation;
